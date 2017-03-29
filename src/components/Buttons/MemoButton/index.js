e1ff4ae343246ad687e25ed20dc5bd7acbcc871a
Daeho Kim <oh3vci@gmail.com>
2017-03-29 23:15:18 +0900

    Modify temporary image

--- src/components/Buttons/MemoButton/index.js
+++ src/components/Buttons/MemoButton/index.js
@@ -5,8 +5,7 @@ export default createInlineStyleButton({
   style: 'MEMO',
   children: (
     <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.6 10.79c.97-.67 1.65-1.77 1.65-2.79 0-2.26-1.75-4-4-4H7v14h7.04c2.09 0 3.71-1.7 3.71-3.79 0-1.52-.86-2.82-2.15-3.42zM10 6.5h3c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-3v-3zm3.5 9H10v-3h3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z" />
-      <path d="M0 0h24v24H0z" fill="none" />
+      <path d='M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1 0 1.71-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z' fill='currentColor' fillRule='evenodd' />
     </svg>
   ),
 });
