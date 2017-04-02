46e1810650890a39efb8d28c01ba8954c8511d61
Daeho Kim <oh3vci@gmail.com>
2017-04-02 14:44:17 +0900

    Create Memo Button util

--- src/utils/createMemoButton.js
+++ src/utils/createMemoButton.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-import unionClassNames from 'union-class-names';
 
-export default ({ alignment, children }) => (
-  class BlockAlignmentButton extends Component {
+export default ({ children }) => (
+  class memoButton extends Component {
 
     activate = (event) => {
       event.preventDefault();
-      this.props.setAlignment({ alignment });
+      event.stopPropagation();
+      this.props.addMemo();
     }
 
     preventBubblingUp = (event) => { event.preventDefault(); }
 
-    isActive = () => this.props.alignment === alignment;
-
     render() {
-      const className = this.isActive() ? unionClassNames("button", "active") : "button";
       return (
         <div
           className="buttonWrapper"
           onMouseDown={this.preventBubblingUp}
         >
           <button
-            className={className}
+            className="button"
             onClick={this.activate}
             type="button"
             children={children}
