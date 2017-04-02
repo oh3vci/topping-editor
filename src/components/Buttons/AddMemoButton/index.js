0908b351f875bcbbae15a420aed1a76b50e0ffad
Daeho Kim <oh3vci@gmail.com>
2017-04-02 14:46:14 +0900

    Remove MemoButton & Create AddMemoButton

---
+++ src/components/Buttons/AddMemoButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import createMemoButton from '../../../utils/createMemoButton';
+
+export default createMemoButton({
+  children: (
+    <span>✓</span>
+  ),
+});
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -6,12 +6,11 @@ import UnderlineButton from './UnderlineButton';
 import FontSizeDownButton from './FontSizeDownButton';
 import FontSizeUpButton from './FontSizeUpButton';
 import ForeColorButton from './ForeColorButton';
-import MemoButton from './MemoButton';
 import AlignLeftButton from './AlignLeftButton';
 import AlignCenterButton from './AlignCenterButton';
 import AlignRightButton from './AlignRightButton';
 import AlignDefaultButton from './AlignDefaultButton';
-
+import AddMemoButton from './AddMemoButton';
 
 export {
   createBlockStyleButton,
@@ -22,9 +21,9 @@ export {
   FontSizeDownButton,
   FontSizeUpButton,
   ForeColorButton,
-  MemoButton,
   AlignLeftButton,
   AlignCenterButton,
   AlignRightButton,
   AlignDefaultButton,
+  AddMemoButton,
 };
