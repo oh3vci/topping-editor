be9cef6958bdceaf9dd3f1dff90a9232fead8ec5
Daeho Kim <oh3vci@gmail.com>
2017-05-28 22:10:44 +0900

    Connect to complete modal

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import { convertToRaw } from 'draft-js';
-import { Button, Icon } from 'semantic-ui-react'
+
 
 class SubmitButton extends React.Component {
 
+  constructor() {
+    super();
+
+    this.submit = this.submit.bind(this);
+  }
+
   submit = () => {
-    const { editorState } = this.props;
+    const { editorState, openModal } = this.props;
     const raw = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
     const essayId = document.getElementById("essayId").innerHTML;
-
 
     axios({
       method: 'post',
@@ -26,16 +31,17 @@ class SubmitButton extends React.Component {
       }
     })
     .then((response) => {
-      alert("저장이 완료되었습니다.");
+      openModal();
     })
     .catch((error) => {
-      alert("저장이 실패하였습니다.");
+      alert("저장에 실패했습니다!");
     });
   }
 
+
   render() {
     return (
-      <div className="submit_button">
+      <div className="submit-button">
         <button onClick={this.submit}>
           저장하기
         </button>
