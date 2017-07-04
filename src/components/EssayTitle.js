709d1821301fd1e35c10b8989eb338e29318f736
Daeho Kim <oh3vci@gmail.com>
2017-07-04 19:24:53 +0900

    Modify to show blank when essay title is modified form

--- src/components/EssayTitle.js
+++ src/components/EssayTitle.js
@@ -1,49 +1,26 @@
 import React from 'react';
-import axios from 'axios';
 
 class EssayTitle extends React.Component {
 
   state = {
     isChange: false,
-    title: '',
+    title: this.props.editorTitle,
   }
 
-  submit = () => {
-    const essayId = document.getElementById("essayId").innerHTML;
-    const { title } = this.state;
-
-    axios({
-      method: 'post',
-      url: '/editor/saveTitle',
-      data: {
-        "title": title,
-        "essayId": essayId
-      },
-      xsrfCookieName: 'csrftoken',
-      xsrfHeaderName: 'X-CSRFToken',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/multipart/form-data; charset=UTF-8'
-      }
-    })
-    .then((response) => {
-      alert("저장이 완료되었습니다.");
-      this.changeFrom();
-    })
-    .catch((error) => {
-      alert("저장이 실패하였습니다.");
-    });
+  changeTitle = (evt) => {
+    this.props.changeTitle(this.state.title);
+    this.changeForm();
   }
 
   changeText = (evt) => {
     this.setState({ title: evt.target.value });
   };
 
-  onKeyDown(e) {
+  onKeyDown = (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
       e.stopPropagation();
-      this.submit();
+      this.changeTitle();
     }
   }
 
@@ -65,9 +42,11 @@ class EssayTitle extends React.Component {
   render() {
     const { isChange } = this.state;
     let { editorTitle } = this.props;
+    /*
     if (editorTitle.length > 40) {
       editorTitle = `${editorTitle.slice(0,40)}...`;
     }
+    */
     return (
       <div className="wrapper-essay-title">
         {
@@ -80,25 +59,14 @@ class EssayTitle extends React.Component {
               type="text"
               onKeyDown={(e) => this.onKeyDown(e)}
               onChange={this.changeText}
+              onBlur={this.changeTitle}
               value={this.state.title}
             />
-            <svg version="1.1" onClick={this.submit} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
-              <path d="M12,1.437C6.169,1.437,1.438,6.169,1.438,12c0,5.831,4.732,10.563,10.563,10.563
-                c5.83,0,10.563-4.732,10.563-10.563C22.563,6.169,17.83,1.437,12,1.437z M18.363,7.31l-6.896,10.426
-                c-0.161,0.246-0.431,0.398-0.718,0.424c-0.025,0-0.042,0-0.068,0c-0.27,0-0.524-0.111-0.701-0.313l-4.259-4.766
-                c-0.347-0.389-0.313-0.99,0.076-1.336c0.389-0.346,0.988-0.313,1.335,0.076l3.439,3.862l6.219-9.405
-                c0.287-0.439,0.871-0.558,1.311-0.271C18.531,6.288,18.65,6.871,18.363,7.31z"/>
-            </svg>
-            <svg version="1.1" onClick={this.changeForm} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
-              <path d="M11.999,1.438C6.166,1.438,1.438,6.167,1.438,12s4.729,10.562,10.562,10.562S22.563,17.834,22.563,12
-              	S17.832,1.438,11.999,1.438z M18.688,17.008l-1.683,1.682L12,13.684l-5.007,5.006l-1.683-1.682L10.318,12L5.312,6.994l1.682-1.683
-              	L12,10.318l5.008-5.006l1.682,1.682L13.683,12L18.688,17.008z"/>
-            </svg>
           </div>
 
           :
           <div className="default-essay-title">
-            <div className="essay-title">
+            <div className="essay-title" onClick={this.changeForm}>
               {
                 editorTitle === ''
                 ?
@@ -106,16 +74,23 @@ class EssayTitle extends React.Component {
                 :
                 editorTitle
               }
-            </div>
-            <div className="change-form" onClick={this.changeForm}>
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
+              &nbsp;
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
                 <g>
-                  <polygon points="6.938,14.889 9.125,17.072 15.737,10.569 13.441,8.273 	"/>
-                  <polygon points="6.392,15.379 6.392,17.621 8.632,17.621 8.741,17.457 6.555,15.27 	"/>
-                  <path d="M12,1.438C6.167,1.438,1.438,6.167,1.438,12c0,5.833,4.729,10.562,10.562,10.562
-                    c5.833,0,10.563-4.729,10.563-10.562C22.563,6.167,17.832,1.438,12,1.438z M18.416,9.367l-9.238,9.182H5.462v-3.715l9.183-9.238
-                    c0.219-0.219,0.547-0.165,0.71,0l3.062,3.06C18.58,8.875,18.58,9.203,18.416,9.367z"/>
-                  <rect x="13.923" y="7.653" transform="matrix(-0.7072 -0.707 0.707 -0.7072 20.5548 25.4425)" width="3.247" height="1.623"/>
+                	<path fillRule="evenodd" clipRule="evenodd" d="M0,24.025c-0.008-0.111-0.025-0.224-0.025-0.335
+                		c-0.001-1.983-0.007-3.968,0.008-5.952c0.001-0.178,0.09-0.399,0.214-0.524c4.233-4.249,8.476-8.488,12.717-12.729
+                		c0.079-0.078,0.161-0.151,0.207-0.195c2.197,2.197,4.381,4.376,6.581,6.576c-0.069,0.072-0.158,0.17-0.251,0.264
+                		c-4.202,4.201-8.403,8.403-12.604,12.603c-0.095,0.097-0.188,0.195-0.282,0.292C4.377,24.025,2.189,24.025,0,24.025z M3.413,16.839
+                		c-0.438,0.439-0.876,0.876-1.31,1.318c-0.051,0.052-0.105,0.132-0.106,0.199c-0.009,0.526-0.005,1.054-0.005,1.624
+                		c0.693,0,1.351,0,2.039,0c0,0.693,0,1.344,0,2.02c0.549,0,1.066,0.002,1.583-0.002c0.052,0,0.12-0.013,0.153-0.046
+                		c0.465-0.467,0.925-0.937,1.397-1.417C5.909,19.298,4.675,18.082,3.413,16.839z M14.02,7.351c-0.01-0.354-0.206-0.501-0.441-0.407
+                		c-0.119,0.048-0.228,0.141-0.322,0.235c-2.768,2.762-5.534,5.527-8.298,8.292c-0.079,0.079-0.193,0.163-0.209,0.257
+                		c-0.023,0.139-0.021,0.343,0.063,0.422c0.082,0.078,0.284,0.06,0.423,0.034c0.085-0.016,0.157-0.119,0.229-0.19
+                		c0.71-0.709,1.419-1.418,2.128-2.127c2.049-2.047,4.099-4.093,6.145-6.143C13.865,7.594,13.958,7.432,14.02,7.351z"/>
+                	<path fillRule="evenodd" clipRule="evenodd" d="M18.612,0c0.66,0.198,1.14,0.648,1.608,1.125
+                		c0.994,1.014,2.007,2.008,3.009,3.014c1.024,1.026,1.027,2.177,0.007,3.198c-0.812,0.811-1.624,1.62-2.407,2.4
+                		c-2.193-2.192-4.372-4.373-6.572-6.572c0.019-0.02,0.078-0.098,0.146-0.165c0.654-0.653,1.318-1.296,1.96-1.959
+                		C16.8,0.592,17.25,0.178,17.868,0C18.115,0,18.365,0,18.612,0z"/>
                 </g>
               </svg>
             </div>
